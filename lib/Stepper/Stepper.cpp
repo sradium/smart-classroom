bf6ceb594cea8fbe93a26498c3b060d50bd0eda7
sradium <sduran@correo.unimet.edu.ve>
2021-01-08 18:19:06 -0400

    first commit

---
+++ lib/Stepper/Stepper.cpp
@@ -0,0 +1,70 @@
+/*!
+ *  @file Stepper.cpp
+ *
+ *  @mainpage Stepper motor with l297 driver.
+ *
+ *  @section intro_sec Introduction TODO: Change description
+ *
+ *  This is a library for DHT series of low cost temperature/humidity sensors.
+ *
+ *  You must have Adafruit Unified Sensor Library library installed to use this
+ * class.
+ *
+ *  Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit andopen-source hardware by purchasing products
+ *  from Adafruit!
+ *
+ *  @section author Author
+ *
+ *  Written by sradium.
+ *
+ */
+
+#include "Stepper.h"
+
+STEPPER::STEPPER(int8_t pin_on, int8_t pin_dir) {
+    _pin_on = pin_on;
+    _pin_dir = pin_dir;
+    is_move = false; 
+    down = LOW;
+}
+
+void STEPPER::begin(){
+    pinMode(_pin_on, OUTPUT);
+    pinMode(_pin_dir, OUTPUT);
+    digitalWrite(_pin_on, LOW);
+    digitalWrite(_pin_dir, down);
+}
+
+bool STEPPER::start_move(bool condition) {
+    if(condition && !is_move){
+        digitalWrite(_pin_on, HIGH);
+        is_move = true;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void STEPPER::change_direction(){
+    if(down == HIGH){
+        down = LOW;
+    } else {
+        down = HIGH;
+    }
+    digitalWrite(_pin_dir, down);
+}
+
+bool STEPPER::stop_move(bool condition){
+    if (condition && is_move){
+        digitalWrite(_pin_on, LOW);
+        is_move = false;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool STEPPER::is_stop(){
+    return !is_move;
+}
---
+++ lib/Stepper/Stepper.h
@@ -0,0 +1,35 @@
+/*!
+ *  @file Stepper.h
+ *
+ *  This is a library for stepper motor series with L297 driver and harware clock.
+ *
+ *  Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit andopen-source hardware by purchasing products
+ *  from Adafruit!
+ *
+ *  Written by sradium.
+ *
+ */
+
+#ifndef Stepper_H
+#define Stepper_H
+
+#include "Arduino.h"
+
+class STEPPER
+{
+public:
+    STEPPER(int8_t pin_on, int8_t pin_dir);
+    void begin();
+    bool start_move(bool condition);
+    bool stop_move(bool condition);
+    void change_direction();
+    bool is_stop();
+private:
+    int8_t _pin_on; 
+    int8_t _pin_dir; 
+    bool is_move; 
+    uint8_t down;
+};
+
+#endif
